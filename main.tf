cb26035ffd5ad162fde3e84c7b958b6d8fa9d4e7
Kabir Kalsi <kabir@vmstechs.com>
2023-03-02 16:20:59 +0530

    Initialize Terraform Remote Backend
    Signed-off-by: Kabir Kalsi <kabir@vmstechs.com>

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "company"
+    organization = "asparkoffire"
 
     workspaces {
-      name = "my-app-prod"
+      name = "terraform-aws-instance-example"
     }
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "<=4.56.0"
-      }
-    }
-  }
 }
-
+}
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
